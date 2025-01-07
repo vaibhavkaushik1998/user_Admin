5662c5568c5530d62440865d08fb0a490b4859cf
vaibhavkaushik1998 <75692661+vaibhavkaushik1998@users.noreply.github.com>
2025-01-07 03:01:39 -0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,37 +1,31 @@
-from django.urls import path
-from user.views import ResUserRegistrationAPI, ResAdminAPI
+"""
+URL configuration for taraerp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    # User API Endpoints
-    path('user/registration/', ResUserRegistrationAPI.as_view(), name='user_registration_api'),  # POST for user registration
-    path('user/update/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_update_api'),  # PUT for updating a user
-    path('user/delete/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_delete_api'),  # DELETE for deleting a user
-    path('user/list/', ResUserRegistrationAPI.as_view(), name='user_list_api'),  # GET for all users
-    path('user/detail/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_detail_api'),  # GET for single user
-
-    # Admin API Endpoints
-    path('resadmin/registration/', ResAdminAPI.as_view(), name='admin_registration_api'),  # POST for admin registration
-    path('resadmin/update/<int:id>/', ResAdminAPI.as_view(), name='admin_update_api'),  # PUT for updating an admin
-    path('resadmin/delete/<int:id>/', ResAdminAPI.as_view(), name='admin_delete_api'),  # DELETE for deleting an admin
-    path('resadmin/list/', ResAdminAPI.as_view(), name='admin_list_api'),  # GET for all admins
-    path('resadmin/detail/<int:id>/', ResAdminAPI.as_view(), name='admin_detail_api'),  # GET for single admin
+    path('admin/', admin.site.urls),
+     path('auditlog/', include('auditlog.urls')),
+     path('customer/', include('customer.urls')),
+     path('notification/', include('notification.urls')),
+     path('order/', include('order.urls')),
+     path('orderitem/', include('orderitem.urls')),
+     path('payment/', include('payment.urls')),
+     path('report/', include('report.urls')),
+     path('user/', include('user.urls')),
+     path('product/', include('product.urls')),
 ]
-
-# from django.urls import path
-# from user.views import ResUserRegistrationAPI, ResAdminAPI
-
-# urlpatterns = [
-#     # Customer API Endpoints
-#     path('registration/api/', ResUserRegistrationAPI.as_view(), name='user_registration_api'),  # POST for registration
-#     path('change/api/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_update_api'),  # PUT for updating user
-#     path('userdelete/api/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_delete_api'),  # DELETE for deleting user
-#     path('get/api/', ResUserRegistrationAPI.as_view(), name='user_list_api'),  # GET for all users
-#     path('get/api/<int:id>/', ResUserRegistrationAPI.as_view(), name='user_detail_api'),  # GET for single user
-
-#     # Admin API Endpoints
-#     path('resadmin/api/', ResAdminAPI.as_view(), name='admin_registration_api'),  # POST for admin registration
-#     path('changeadmin/api/<int:id>/', ResAdminAPI.as_view(), name='admin_update_api'),  # PUT for updating admin
-#     path('admindelete/api/<int:id>/', ResAdminAPI.as_view(), name='admin_delete_api'),  # DELETE for deleting admin
-#     path('adminget/api/', ResAdminAPI.as_view(), name='admin_list_api'),  # GET for all admins
-#     path('adminget/api/<int:id>/', ResAdminAPI.as_view(), name='admin_detail_api'),  # GET for single admin
-# ]
